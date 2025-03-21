eebec40588348483d7f55b078cddd374bfaba591
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-03-21 14:50:16 +0100

    I added useState and are now  displaying some api data in the UI

--- src/weather/Weather.jsx
+++ src/weather/Weather.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "./Weather.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Weather = () => {
+  const [weather, setWeather] = useState(null);
+
   useEffect(() => {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=ffd76b07ad6c23883f4dd787b3c0d8f5&units=metric&lang=sv"
+      "https://api.openweathermap.org/data/2.5/weather?q=frankrike&appid=ffd76b07ad6c23883f4dd787b3c0d8f5&units=metric&lang=sv"
     )
       .then((response) => response.json())
       .then((data) => {
+        setWeather(data);
         console.log(data);
       })
       .catch((error) => console.error("Error:", error));
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="weather">
+      <h2>Väder i {weather?.name}</h2>
+      <p>{weather?.weather?.[0]?.description}</p>
+      <p>Temperatur: {weather?.main?.temp}</p>
+      <p>Wind: {weather?.wind?.speed}</p>
+    </div>
+  );
 };
 
 export default Weather;
