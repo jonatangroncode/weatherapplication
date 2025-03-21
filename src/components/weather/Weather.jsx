57e7fe57ec705d85c397d22506153a7c0ee6ec49
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-03-21 16:55:19 +0100

    Data control improvement

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -19,16 +19,16 @@ const Weather = () => {
 
   return (
     <div className="weather">
-      {weather && (
+      {weather?.weather?.[0] && (
         <>
           <h2>Väder i {weather.name}</h2>
-          <p>{weather.weather?.[0]?.description}</p>
-          <p>Temperatur: {weather.main?.temp}°C</p>
-          <p>Wind: {weather.wind?.speed}</p>
+          <p>Väder beskrivning: {weather.weather[0].description}</p>
+          <p>Temperatur: {weather.main?.temp ?? "Okänd"}°C</p>
+          <p>Wind: {weather.wind?.speed ?? "Okänd"}</p>
 
           <img
-            src={`https://openweathermap.org/img/wn/${weather.weather[0]?.icon}@2x.png`}
-            alt={weather?.weather[0]?.description}
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
           />
         </>
       )}
