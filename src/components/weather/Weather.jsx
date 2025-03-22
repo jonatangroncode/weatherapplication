6567a94a3457de6540d80f63cf69dd73bb13513c
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-03-22 22:25:25 +0100

    Functionality to toggle a state  staricon

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import "./Weather.css";
 import { useEffect, useState } from "react";
+import starFilled from "../../images/star-filled.png";
+import starEmpty from "../../images/star-empty.png";
 
 const Weather = () => {
   const [weather, setWeather] = useState(null);
   const [coords, setCordinates] = useState(null);
   const [city, setCity] = useState("");
   const [searchCity, setSearchCity] = useState(null);
+  const [favorites, setFavorites] = useState([
+    {
+      id: 1,
+      name: "Stockholm",
+      temperature: 20,
+      description: "Kallt",
+      isFavorited: true,
+    },
+  ]);
+
+  let starIcon = favorites.isFavorited ? starFilled : starEmpty;
+
+  function toggleFavorite() {
+    setFavorites((prevFavorites) => ({
+      ...prevFavorites,
+      isFavorited: !prevFavorites.isFavorited,
+    }));
+  }
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -60,30 +80,40 @@ const Weather = () => {
   };
 
   return (
-    <div className="weather">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Sök Ort, Stad, land.."
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button type="submit">Sök</button>
-      </form>
-      {weather?.weather?.[0] && (
-        <>
-          <h2>Väder i {weather.name}</h2>
-          <p>Väder beskrivning: {weather.weather[0].description}</p>
-          <p>Temperatur: {weather.main?.temp ?? "Okänd"}°C</p>
-          <p>Wind: {weather.wind?.speed ?? "Okänd"}</p>
-
-          <img
-            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-            alt={weather.weather[0].description}
+    <>
+      <header>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Sök Ort, Stad, land.."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
           />
-        </>
-      )}
-    </div>
+          <button type="submit">Sök</button>
+        </form>
+      </header>
+      <main>
+        <article className="card">
+          <button onClick={toggleFavorite} className="favorite-button">
+            <img src={starIcon} alt="favorit" />
+          </button>
+          {weather?.weather?.[0] && (
+            <>
+              <h2>Väder i {weather.name}</h2>
+              <p>Väder beskrivning: {weather.weather[0].description}</p>
+              <p>Temperatur: {weather.main?.temp ?? "Okänd"}°C</p>
+              <p>Wind: {weather.wind?.speed ?? "Okänd"}</p>
+
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+              />
+              <button>Mer info</button>
+            </>
+          )}
+        </article>
+      </main>
+    </>
   );
 };
 
